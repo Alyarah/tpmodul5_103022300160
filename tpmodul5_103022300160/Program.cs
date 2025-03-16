ffc25886cea3f2a0812584cb1b46de3cc4f42da0
Alyaras <alyarahmadayanis@gmail.com>
2025-03-17 00:24:42 +0700

    menambahkan class DataGeneric

--- tpmodul5_103022300160/Program.cs
+++ tpmodul5_103022300160/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+using tpmodul5_103022300160;
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> NIM = new DataGeneric<string>("103022300160");
+        NIM.PrintData();
+    }
+}
